a486d19577f0d6ed35a21254f1b8e5d5f249bf84
Paul Chevrier <paul.chevrier@gmail.com>
2021-02-11 18:46:11 +0100

    Update project name

--- fuel-pump/Properties/AssemblyInfo.cs
+++ fuel-pump/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Les informations générales relatives à un assembly dépendent de
 // l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
 // associées à un assembly.
-[assembly: AssemblyTitle("Pompe a essence")]
+[assembly: AssemblyTitle("fuel-pump")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Pompe a essence")]
+[assembly: AssemblyProduct("fuel-pump")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
